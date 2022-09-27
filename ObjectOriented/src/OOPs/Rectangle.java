a5abca07918afe882a8c63bd514de1aacbb228c5
Shubham Khule <shubham.khule@kpipartners.com>
2022-09-26 18:29:24 -0700

    area square rectangle

---
+++ ObjectOriented/src/OOPs/Rectangle.java
@@ -0,0 +1,13 @@
+package OOPs;
+
+public class Rectangle extends Area{
+	
+	public static void main(String args[])
+    {
+		
+		Rectangle rectangle = new Rectangle();
+        rectangle.Rectangle();
+        
+
+}
+}
